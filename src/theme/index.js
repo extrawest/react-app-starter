2f1374fa34965ed1363d424ade04ea9e7a443fbb
Dmytro Podolianskyi <dmytro.podolianskyi@extrawest.com>
2021-06-27 15:22:10 +0300

    Updated theme colors

--- src/theme/index.js
+++ src/theme/index.js
@@ -6,7 +6,6 @@ const theme = createMuiTheme({
     fontFamily: "Montserrat, sans-serif",
     h1: {
       fontFamily: "Montserrat, sans-serif",
-      color: "#fff",
       fontSize: 76,
       fontWeight: 900,
       lineHeight: "92px",
@@ -16,7 +15,6 @@ const theme = createMuiTheme({
       fontSize: 54,
       fontWeight: 700,
       lineHeight: "64px",
-      color: "#fff",
     },
     h3: {
       fontFamily: "Montserrat, sans-serif",
@@ -24,7 +22,6 @@ const theme = createMuiTheme({
       fontWeight: 800,
       lineHeight: "22px",
       letterSpacing: "0.02em",
-      color: "#fff",
     },
     h4: {
       fontFamily: "Montserrat, sans-serif",
@@ -32,7 +29,6 @@ const theme = createMuiTheme({
       fontWeight: 600,
       lineHeight: "22px",
       letterSpacing: "0.0038em",
-      color: "#fff",
     },
     h5: {
       fontFamily: "Montserrat, sans-serif",
@@ -40,7 +36,6 @@ const theme = createMuiTheme({
       fontWeight: 700,
       lineHeight: "20px",
       letterSpacing: "0.02em",
-      color: "#fff",
     },
     h6: {
       fontFamily: "Montserrat, sans-serif",
@@ -48,25 +43,21 @@ const theme = createMuiTheme({
       fontWeight: 500,
       lineHeight: "18px",
       letterSpacing: "-0.00615385em",
-      color: "#fff",
     },
     body1: {
       fontFamily: "Montserrat, sans-serif",
       fontSize: 16,
       lineHeight: "20px",
       letterSpacing: "0.02em",
-      color: "#fff"
     },
     body2: {
       fontFamily: "Montserrat, sans-serif",
       fontSize: 17,
       lineHeight: "28px",
       letterSpacing: "0.02em",
-      color: "#C0C0C0"
     },
     caption: {
       fontFamily: "Montserrat, sans-serif",
-      color: "#fff",
       fontSize: 11,
       lineHeight: "18px",
       letterSpacing: "-0.00615385em",
@@ -80,7 +71,7 @@ const theme = createMuiTheme({
       main: "#8E2DE2",
     },
     background: {
-      default: "#181B48",
+      default: "#fff",
     },
   },
 });
@@ -91,13 +82,16 @@ theme.overrides = {
       fontSize: 20,
       lineHeight: "20px",
       color: theme.palette.common.white,
-      padding: "30px 80px",
+      padding: "16px 40px",
       background: "linear-gradient(93.72deg, #8E2DE2 9.41%, #4A00E0 86.1%)",
       borderRadius: 50,
       minWidth: "none",
       maxWitdh: "none",
       textTransform: "none"
     },
+    text: {
+      padding: "16px 40px",
+    }
   },
   MuiMenuItem: {
     root: {
